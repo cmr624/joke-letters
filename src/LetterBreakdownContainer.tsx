2be00ab7cc7a90e905b0a3e87210c8a4176ec352
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-10-03 23:10:38 -0400

    checks

--- src/LetterBreakdownContainer.tsx
+++ src/LetterBreakdownContainer.tsx
@@ -1,6 +1,7 @@
 //@ts-ignore
 import React, {useState, useEffect} from 'react';
 import TextField from '@material-ui/core/TextField';
+import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, FormHelperText } from '@material-ui/core';
 const LetterBreakdownContainer = () => {
 
     const [currentJoke, setCurrentJoke ] = useState('');
@@ -39,21 +40,61 @@ const LetterBreakdownContainer = () => {
             />
         </form>
         <h2>RESULTS</h2>
-        <ul>
+        <div>
             {
             //@ts-ignore
-            currentLetterList.map((e) => <li><b>{e.letter} = {e.count}</b></li>)
+            currentLetterList.map((e) =>
+            <div>
+                <LetterBreakdownComponent {...e}/>
+            </div>
+            )
             }
-        </ul>
+        </div>
         </>
     );
 }
 
 export default LetterBreakdownContainer;
 
+interface LetterBreakdownComponentProps {
+    letter : string,
+    count : string
+}
+
+interface LetterBreakdownListProps {
+    list : {letter:string,count:string}[]
+}
+
+function LetterBreakdownList(props : LetterBreakdownListProps){
+    return<>
+        <FormControl component="fieldset" style={{margin:'auto'}}>
+        <FormLabel component="legend">Assign responsibility</FormLabel>
+        <FormGroup>
+         {props.list.map((e : any, i : number)=> 
+            <LetterBreakdownComponent 
+                {...props.list[i]}
+            />)} 
+         
+        </FormGroup>
+        <FormHelperText>Be careful</FormHelperText>
+      </FormControl>
+    </>
+}
+
+function LetterBreakdownComponent(props : LetterBreakdownComponentProps){
+
+    const [checked, setChecked] = useState(false);
+
+    return <FormControlLabel
+        control={<Checkbox checked={checked} onChange={(e : any) => 
+        setChecked(e.target.checked)
+        } name="key" />}
+        label={`${props.letter} - ${props.count}`}
+    />
+}
+
 function stringToArrayWithCharacterCounts(s : string) {
     //@ts-ignore
     let result = [...s].reduce((a : string, e : string) => { a[e] = a[e] ? a[e] + 1 : 1; return a }, {}); 
-    console.log(result);
     return result;
 }
